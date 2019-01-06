a861cd16c4b1e4ba45fc0f2ea37269d77ed50864
Keith Berry <keithwberry@gmail.com>
2019-01-06 19:06:07 +0000

    Working changes to models

--- download-data.r
+++ download-data.r
@@ -13,22 +13,23 @@ load.packages('quantmod')
 # SPY,QQQ,IWM,VGK,EWJ,VWO,GSG,GLD,VNQ,HYG, LQD,TLT + C2:SHY,IEF
 
 tickers = '
-SPY = SPY + ^GSPC, #S&P500 (VOO)
-EFA = EFA + VTMGX, #Developed markets
+VOO = VOO + VFINX, #S&P500 (VOO) - 1980
+VEA = VEA + VGTSX, #Developed markets - Aug 1 1999 (Could use VGTSX goes back to 94 or index)
+#VEA = VEA + VTMGX, #Developed markets - Aug 1 1999 (Could use VGTSX goes back to 94 or index)
 #QQQ = QQQ + ^NDX, #Nasdaq
 #IWM = IWM + ^RUT, #Russell 2000
-#VGK = VGK + FIEUX, #FTSE Europe
+#VGK = VGK + FIEUX, #FTSE Europe 
 #EWJ = EWJ + FJPNX, #MSCI Japan
-AGG = AGG + VBMFX, #Total bonds
-VWO = VWO + VEIEX, #FTSE Emerging Markets
+BND = BND + VBMFX, #Total bonds
+VWO = VWO + VEIEX, #FTSE Emerging Markets 1 May 1994
 #GSG = GSG + , #Commodities
 #GLD = GLD + , #Gold
 #VNQ = VNQ + VGSIX, #Real Estate
 #HYG = HYG + , #High Yield Corporate
-#LQD = LQD + , #Investment Grade Corporate
-TLT = TLT + VUSTX, #20+ Year Treasury
-SHY = SHY + VFISX, #1 to 3 Year Treasury
-IEF = IEF + VFITX, #7 to 10 Year Treasury
+LQD = LQD + VWESX, #Investment Grade Corporate - Jan 1980
+TLT = TLT + VUSTX, #20+ Year Treasury - June 86
+SHY = SHY + VFISX, #1 to 3 Year Treasury - Oct 91
+IEF = IEF + VFITX, #7 to 10 Year Treasury - Nov 91
 '
 #             TLT = TLT + VUSTX,
 # #              TLT = BLV + VBLTX,
@@ -75,5 +76,6 @@ IEF = IEF + VFITX, #7 to 10 Year Treasury
 raw <- new.env()
 
 getSymbols.extra(tickers, src='yahoo', from = '1970-01-01', env = raw, set.symbolnames = T, auto.assign = T)
+for(i in raw$symbolnames) raw[[i]] = adjustOHLC(raw[[i]], use.Adjusted = TRUE, symbol.name=i)
 
 saveRDS(raw, file="./data/raw-prices.RData")
